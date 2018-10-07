635661665dbc089542df099462b76e54e054ea99
Sille Van Landschoot <info@sillevl.be>
2018-10-07 13:49:17 +0200

    migrate to vue

--- js/app.js
+++ js/app.js
@@ -1,66 +1,72 @@
-let ledCharacteristic = null;
+var app = null;
 
-window.addEventListener('load', () => {
-    console.log("Staring application");
+console.log(navigator.bluetooth)
 
-    let button = document.getElementById('connect');
-    button.addEventListener('click', connect);
-
-    let sendButton = document.getElementById('send');
-    sendButton.addEventListener('click', function(){
+document.addEventListener('DOMContentLoaded', () => {
+   app = new Vue({
+    el: '#app',
+    data: {
+      color: {red: 0, green: 128, blue: 255},
+      presets: [],
+      ledCharacteristic: null
+    },
+    mounted() {
+      if (localStorage.getItem('color')) {
+        this.color = JSON.parse(localStorage.getItem('color'))
+      }
+    },
+    computed: {
+      colorStyle() {
+        let c = this.color
+        return {
+          backgroundColor: `rgb(${c.red}, ${c.green}, ${c.blue})`,
+        };
+      }
+    },
+    watch: {
+      color: {
+        handler(newValue, oldValue) {
+          this.sendColor()
+          localStorage.setItem('color',JSON.stringify(newValue))
+        },
+        deep: true 
+      }
+    },
+    methods: {
+      connect: function(){
+        console.log("connect button pressed")
+        navigator.bluetooth.requestDevice({ 
+            filters: [{
+                namePrefix: 'Proto',
+            }],
+            // acceptAllDevices: true, 
+            optionalServices: ['ad11cf40-063f-11e5-be3e-0002a5d5c51b']
+        })
+        .then(device => {
+            console.log(`Connected to: ${device.name}`);
+            return device.gatt.connect();
+        })
+        .then(server => {
+            console.log('Getting primary service')
+            return server.getPrimaryService('ad11cf40-063f-11e5-be3e-0002a5d5c51b');
+        })
+        .then(service => {
+            console.log('Getting Characteristic for rgb led')
+            return service.getCharacteristic('bf3fbd80-063f-11e5-9e69-0002a5d5c503');
+        })
+        .then(charcteristic => {
+            console.log(charcteristic)
+            this.ledCharacteristic = charcteristic;
+        })
+        .catch(error => { console.log(error.name, error.message); });
+      },
+      sendColor: function(){
         let view = new Uint8Array(3);
-        view[0] = 0x00;
-        view[1] = 0x00;
-        view[2] = 0xFF;
-        ledCharacteristic.writeValue(view);
-    });
-});
-
-function handleCharacteristicValueChanged(event) {
-    console.log("event !!!");
-    var value = event.target.value;
-    console.log('Received ' + value);
-  }
-
-function connect(){
-    navigator.bluetooth.requestDevice({ 
-        filters: [{
-            namePrefix: 'Disco',
-        }],
-        // acceptAllDevices: true, 
-        optionalServices: ['ad11cf40-063f-11e5-be3e-0002a5d5c51b']
-    })
-    .then(device => {
-        // Human-readable name of the device.
-        console.log(device.name);
-
-        // Attempts to connect to remote GATT Server.
-        return device.gatt.connect();
-    })
-    .then(server => {
-        return server.getPrimaryService('ad11cf40-063f-11e5-be3e-0002a5d5c51b');
-    })
-    .then(service => {
-        return service.getCharacteristic('bf3fbd80-063f-11e5-9e69-0002a5d5c503');
-    })
-    .then(charcteristic => {
-        ledCharacteristic = charcteristic;
-    })
-    // .then(service => {
-    //     return service.getCharacteristic('bf3fbd80-063f-11e5-9e69-0002a5d5c501');
-    // })
-    // .then(characteristic => {
-    //     return characteristic.readValue();
-    // })
-    // .then(value => {
-    //     console.log(value.byteLength);
-    //     console.log('> Temperature: ' + value.getInt16());
-    // })
-    // .then(characteristic => {return characteristic.startNotifications()})
-    // .then(characteristic => {
-    //     characteristic.addEventListener('characteristicvaluechanged', handleCharacteristicValueChanged);
-    //     console.log('Notifications have been started.');
-    // })
-    
-    .catch(error => { console.log(error); });
-}
+        view[0] = this.color.red;
+        view[1] = this.color.green;
+        view[2] = this.color.blue;
+        this.ledCharacteristic.writeValue(view);
+      }
+    }
+  })
+})
